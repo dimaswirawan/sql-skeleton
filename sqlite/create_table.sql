92c408c7aad7830e71066aaa817bc0721fbfbaa0
Dimas Wirawan <dimas_jumbo@yahoo.com>
2024-01-01 09:14:13 +0700

    Update create_table.sql

--- sqlite/create_table.sql
+++ sqlite/create_table.sql
@@ -11,3 +11,44 @@ CREATE TABLE IF NOT EXISTS "data" (
 	"data_modified"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
+
+CREATE TABLE IF NOT EXISTS "datameta" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"data_id"	INTEGER,
+	"meta_key"	TEXT,
+	"meta_value"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT "term_relationships" (
+	"object_id"	INTEGER,
+	"term_taxonomy_id"	INTEGER,
+	"term_order"	INTEGER,
+	PRIMARY KEY("object_id")
+);
+
+CREATE TABLE IF NOT "terms" (
+	"term_id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT,
+	"slug"	TEXT,
+	"term_group"	TEXT,
+	PRIMARY KEY("term_id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT "termmeta" (
+	"meta_id"	INTEGER NOT NULL UNIQUE,
+	"term_id"	INTEGER,
+	"meta_key"	TEXT,
+	"meta_value"	TEXT,
+	PRIMARY KEY("meta_id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT "term_taxonomy" (
+	"term_taxonomy_id"	INTEGER NOT NULL UNIQUE,
+	"term_id"	INTEGER,
+	"taxonomy"	TEXT,
+	"description"	TEXT,
+	"parent"	INTEGER,
+	"count"	INTEGER,
+	PRIMARY KEY("term_taxonomy_id" AUTOINCREMENT)
+);
